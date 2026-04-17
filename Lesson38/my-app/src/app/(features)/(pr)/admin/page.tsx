5ffd937fdbb11747b0637134292d20df9a7767dd
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2026-04-18 00:24:07 +0400

    chore(next.js): Add Project

---
+++ Lesson38/my-app/src/app/(features)/(pr)/admin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const adminPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <p>Welcome to the admin panel!</p>
+    </div>
+  )
+}
+
+export default adminPage
